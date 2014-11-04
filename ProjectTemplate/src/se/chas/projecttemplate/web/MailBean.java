c02b2ce78f8058dd5f50dc6863af72bb15bcdc59
Tommy801008 <tommy.hallgren@chas.se>
2014-11-04 16:50:31 +0100

    simple TLS/SSL mail methods implemented & tested. they need to be re-written

---
+++ ProjectTemplate/src/se/chas/projecttemplate/web/MailBean.java
@@ -0,0 +1,94 @@
+package se.chas.projecttemplate.web;
+
+import java.util.Properties;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@ManagedBean
+@ViewScoped
+public class MailBean {
+
+	public MailBean() {
+		System.out.println("in constructor");
+	}
+	
+	public void sendMailTLS() {
+		final String username = "tommy801008test@gmail.com";
+		final String password = "certinads123";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("from-email@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("tommy.hallgren@chas.se"));
+			message.setSubject("Testing Subject TLS");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void sendMailSSL() {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+ 
+		Session session = Session.getDefaultInstance(props,
+			new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("tommy801008test@gmail.com", "certinads123");
+				}
+			});
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("from@no-spam.com"));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse("tommy.hallgren@chas.se"));
+			message.setSubject("Testing Subject SSL");
+			message.setText("Dear Mail Crawler," +
+					"\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
